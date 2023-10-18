b44559b5fb53cb7e1341cef2196785c07bb0c46f
ivanjorge <ivandcarvalho29@gmail.com>
2023-10-18 18:46:30 +0100

    update

--- netlifly/functions/api.js
+++ netlifly/functions/api.js
@@ -2,14 +2,14 @@ import 'dotenv/config'
 import express, { Router } from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
-import mongoose from "mongoose"
 import serverless from "serverless-http"
-import instructorsRoute from '../routes/instructors.js'
-import studentsRoute from './routes/students.js'
-import sessionRoute from './routes/sessions.js'
-import eventsRoute from './routes/events.js'
-import notificationsRoute from './routes/notifications.js'
-import loginRoute from './routes/login.js'
+// import mongoose from "mongoose"
+// import instructorsRoute from '../routes/instructors.js'
+// import studentsRoute from './routes/students.js'
+// import sessionRoute from './routes/sessions.js'
+// import eventsRoute from './routes/events.js'
+// import notificationsRoute from './routes/notifications.js'
+// import loginRoute from './routes/login.js'
 
 const api = express();
 
@@ -19,19 +19,19 @@ router.get("/hello", (req, res) => res.send("Hello World!"))
 api.use(cors());
 api.use(bodyParser.json())
 
-mongoose.connect(process.env.DATABASE_URL,{
-useNewUrlParser: true,
-useUnifiedTopology: true,
-})
+// mongoose.connect(process.env.DATABASE_URL,{
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
+// })
 
-api.use('/instructors', instructorsRoute)
-api.use('/students', studentsRoute)
-api.use('/sessions', sessionRoute)
-api.use('/events', eventsRoute)
-api.use('/notifications', notificationsRoute)
-api.use('/login', loginRoute)
+// api.use('/instructors', instructorsRoute)
+// api.use('/students', studentsRoute)
+// api.use('/sessions', sessionRoute)
+// api.use('/events', eventsRoute)
+// api.use('/notifications', notificationsRoute)
+// api.use('/login', loginRoute)
 
-api.use("/api/", router);
+// api.use("/api/", router);
 
 const handler = serverless(api)
 export default handler
