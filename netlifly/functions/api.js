bc64546dabd233bb3c1b59a9e74e80a51c518435
ivanjorge <ivandcarvalho29@gmail.com>
2023-10-20 09:30:46 +0100

    update

--- netlifly/functions/api.js
+++ netlifly/functions/api.js
@@ -3,35 +3,36 @@ import serverless from "serverless-http"
 import 'dotenv/config'
 import cors from "cors"
 import bodyParser from "body-parser"
-// import mongoose from "mongoose"
-// import instructorsRoute from '../routes/instructors.js'
-// import studentsRoute from './routes/students.js'
-// import sessionRoute from './routes/sessions.js'
-// import eventsRoute from './routes/events.js'
-// import notificationsRoute from './routes/notifications.js'
-// import loginRoute from './routes/login.js'
+import mongoose from "mongoose"
+import instructorsRoute from '../routes/instructors.js'
+import studentsRoute from './routes/students.js'
+import sessionRoute from './routes/sessions.js'
+import eventsRoute from './routes/events.js'
+import notificationsRoute from './routes/notifications.js'
+import loginRoute from './routes/login.js'
 
 const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"))
 
 api.use(cors());
 api.use(bodyParser.json())
 
-// mongoose.connect(process.env.DATABASE_URL,{
-// useNewUrlParser: true,
-// useUnifiedTopology: true,
-// })
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"))
 
-// api.use('/instructors', instructorsRoute)
-// api.use('/students', studentsRoute)
-// api.use('/sessions', sessionRoute)
-// api.use('/events', eventsRoute)
-// api.use('/notifications', notificationsRoute)
-// api.use('/login', loginRoute)
 
-// api.use("/api/", router);
+mongoose.connect(process.env.DATABASE_URL,{
+useNewUrlParser: true,
+useUnifiedTopology: true,
+})
+
+api.use('/instructors', instructorsRoute)
+api.use('/students', studentsRoute)
+api.use('/sessions', sessionRoute)
+api.use('/events', eventsRoute)
+api.use('/notifications', notificationsRoute)
+api.use('/login', loginRoute)
+
+api.use("/api/", router);
 
 const handler = serverless(api)
 export default handler
